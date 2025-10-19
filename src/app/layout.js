20080b86128ed88e90e74b8b7e4f54c2b1c91281
Hazrat 17880 <hazrat17780@gmail.com>
2025-10-19 16:25:58 +0500

    Added new code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
+import "./../globals.css";
 import Navbar from "@/component/common/NavBar";
 import Footer from "@/component/common/Footer";
 import { ToastContainer } from "react-toastify";
